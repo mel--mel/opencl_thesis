fe144726867c8dbb5e4c653556a3d62a79d2aa4d
mel--mel <p_mel@windowslive.com>
2014-11-07 15:17:04 +0200

    functions transferred to helpingFunctions.cpp

--- SimpleImage/HelpingFunctions.h
+++ SimpleImage/HelpingFunctions.h
@@ -3,5 +3,11 @@
  
 int compfunc(const void *pa, const void *pb);
 
+int getKernelWorkGroupSize(cl_device_id devId, cl_kernel kernelName, size_t workGroupSize);
+
+void histogramEqualization(pixelStruct *colorArray, cl_uint height, cl_uint width);
+
+void setZero(pixelStruct *array, cl_uint height, cl_uint width);
+
  
 #endif
