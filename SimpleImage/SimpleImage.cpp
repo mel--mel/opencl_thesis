5a75c16ad66b5b977cc988974e528a7c0ebe80b7
mel--mel <p_mel@windowslive.com>
2014-11-27 15:49:46 +0200

    unused simpleImage functions eliminated

--- SimpleImage/SimpleImage.cpp
+++ SimpleImage/SimpleImage.cpp
@@ -24,136 +24,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 #define OUTPUT_IMAGE "L_Out.bmp"
 
-int SimpleImage::createBuffer(cl_mem &bufferName, pixelStruct *arrayName){
-
-	int status = 0;
-
-	/*arrayName = (pixelStruct*)calloc(width * height, sizeof(pixelStruct));
-	CHECK_ALLOCATION(arrayName,
-                     "Failed to allocate memory!");
-	*/
-	
-	bufferName = clCreateBuffer(context, 
-		                       CL_MEM_READ_WRITE, 
-							   width * height * sizeof(pixelStruct), 
-							   NULL, 
-							   &status);
-	CHECK_OPENCL_ERROR(status,"clCreateBuffer failed. (redBuffer)");
-
-	status = clEnqueueWriteBuffer(commandQueue,
- 								 bufferName,
- 								 1,
- 								 0,
- 								 width * height * sizeof(pixelStruct),
-								 arrayName,
- 								 0, 0, 0);
-	CHECK_OPENCL_ERROR(status,"clEnqueueWriteBuffer failed. (redBuffer)");
-
-	return CL_SUCCESS;
-}
-
-int SimpleImage::getInputImage(std::string imageName, cl_image_desc *imageDesc, cl_uchar4 **imageData)
-{
-	// Allocate host memoryF and read input image
-    //std::string filePath = getPath() + std::string(INPUT_IMAGE);
-	CHECK_OPENCL_ERROR(readInputImage(imageName, imageDesc, imageData), "Read Input Image failed");
-
-	//enable timing for this SimpleImage
-	sampleArgs->timing = 1;
-
-	return CL_SUCCESS;
-}
-
-int SimpleImage::readInputImage(std::string imageName, cl_image_desc *imageDesc, cl_uchar4 **imageData)
-{
-
-	//SDKBitMap inputBitmap;
-    // load input bitmap image
-    inputBitmap.load(imageName.c_str());
-
-    // error if image did not load
-    if(!inputBitmap.isLoaded()){
-        error("Failed to load input image!");
-        return SDK_FAILURE;
-    }
-
-    // get width and height of input image
-    height = inputBitmap.getHeight();
-    width = inputBitmap.getWidth();
-
-    // allocate memory for input & outputimage data
-    *imageData = (cl_uchar4*)malloc(width * height * sizeof(cl_uchar4));
-    CHECK_ALLOCATION(imageData,"Failed to allocate memory! (inputImageData)");
-
-    // get the pointer to pixel data
-    pixelData = inputBitmap.getPixels();
-    CHECK_ALLOCATION(pixelData,"Failed to read pixel Data!");
-
-    // Copy pixel data into inputImageData
-    memcpy(*imageData, pixelData, width * height * pixelSize);
-
-    // allocate memory for verification output
-    verificationOutput = (cl_uchar*)malloc(width * height * pixelSize);
-    CHECK_ALLOCATION(pixelData,"verificationOutput heap allocation failed!");
-
-    // initialize the data to NULL
-    //memset(verificationOutput, 0, width * height * pixelSize);
-    memcpy(verificationOutput, *imageData, width * height * pixelSize);
-
-	//parameter imageDesc needed for clGreateImage
-    memset(imageDesc, '\0', sizeof(cl_image_desc));
-    imageDesc->image_type = CL_MEM_OBJECT_IMAGE2D;
-    imageDesc->image_width = width;
-    imageDesc->image_height = height;
-
-    return SDK_SUCCESS;
-
-}
-
-int SimpleImage::createColorArrays()
-{
-	// allocate memory for 1D pixel struct array
-	redArray = (pixelStruct*)calloc(width * height, sizeof(pixelStruct));
-	CHECK_ALLOCATION(redArray, "Failed to allocate memory! (redArray)");
-
-	greenArray = (pixelStruct*)calloc(width * height, sizeof(pixelStruct));
-	CHECK_ALLOCATION(greenArray, "Failed to allocate memory! (greenArray)");
-	
-	blueArray = (pixelStruct*)calloc(width * height, sizeof(pixelStruct));
-	CHECK_ALLOCATION(blueArray, "Failed to allocate memory! (blueArray)");
-
-	return SDK_SUCCESS;
-}
-
-int SimpleImage::writeOutputImage(std::string outputImageName)
-{
-    // copy output image data back to original pixel data
-    memcpy(pixelData, outputImageData2D, width * height * pixelSize);
-
-    // write the output bmp file
-    if(!inputBitmap.write(outputImageName.c_str()))
-    {
-        std::cout << "Failed to write output image!";
-        return SDK_FAILURE;
-    }
-    return SDK_SUCCESS;
-}
-
-int SimpleImage::genBinaryImage()
-{
-    bifData binaryData;
-    binaryData.kernelName = std::string("SimpleImage_Kernels.cl");
-    binaryData.flagsStr = std::string("");
-    if(sampleArgs->isComplierFlagsSpecified())
-    {
-        binaryData.flagsFileName = std::string(sampleArgs->flags.c_str());
-    }
-
-    binaryData.binaryName = std::string(sampleArgs->dumpBinary.c_str());
-    int status = generateBinaryImage(binaryData);
-    return status;
-}
-
 void SimpleImage::setupCL(std::string kernelsFileName)
 {
     cl_int status = CL_SUCCESS;
@@ -218,27 +88,6 @@ void SimpleImage::setupCL(std::string kernelsFileName)
 
 }
 
-int SimpleImage::checkResources(int numOfKernels, cl_kernel *kernelNames){ // Check group size against group size returned by kernel
-
-	cl_device_id devId = devices[sampleArgs->deviceId];
-	
-	size_t minWorkGroupSize = findMinWorkGroupSize(numOfKernels, kernelNames, devId);
-
-    if((blockSizeX * blockSizeY) > minWorkGroupSize) {
-        if(!sampleArgs->quiet){
-            std::cout << "Out of Resources!" << std::endl;
-            std::cout << "Group Size specified : "<< blockSizeX * blockSizeY << std::endl;
-            std::cout << "Max Group Size supported on the kernel(s) : "<< minWorkGroupSize << std::endl;
-            std::cout << "Falling back to " << minWorkGroupSize << std::endl;
-        }
-		if(blockSizeX > minWorkGroupSize){
-            blockSizeX = minWorkGroupSize;
-            blockSizeY = 1;
-        }
-    }
-
-	return CL_SUCCESS;
-}
 
 void SimpleImage::runThisKernel(const char* kernelFileName, size_t *globalThreads, size_t *localThreads, 
 							   cl_mem &buffer1, cl_mem &buffer2, cl_mem &buffer3,
@@ -262,8 +111,6 @@ void SimpleImage::runThisKernel(const char* kernelFileName, size_t *globalThread
 
 }
 
-
-
 void SimpleImage::runThisKernel(const char* kernelFileName, size_t *globalThreads, size_t *localThreads, 
 							   cl_mem &buffer1, cl_mem &buffer2, cl_mem &buffer3,
 							   cl_mem &imageName)
@@ -283,116 +130,15 @@ void SimpleImage::runThisKernel(const char* kernelFileName, size_t *globalThread
     if (status != CL_SUCCESS) throw "clEnqueueNDRangeKernel failed.";
 }
 
-int SimpleImage::runCLKernels()
-{
-    cl_int status;
 
-	// Enqueue a kernel run call.
-    size_t globalThreads[] = {width, height};
-    size_t localThreads[] = {blockSizeX, blockSizeY};
-
-	//Create args
-	inputImage2D = clCreateImage(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, &imageFormat, &imageDesc1, imageData1, &status);
-    CHECK_OPENCL_ERROR(status,"clCreateImage failed. (inputImage2D)");
-
-	CHECK_OPENCL_ERROR(createColorArrays(), "createColorArrays() failed");
-
-	createBuffer(redBuffer, redArray);
-	createBuffer(greenBuffer, greenArray);
-	createBuffer(blueBuffer, blueArray);
-
-	runThisKernel("createColorArrays", globalThreads, localThreads, redBuffer, greenBuffer, blueBuffer, inputImage2D);
-
-	//Copy from buffers to color arrays
-	cl_mem buffers[] = {redBuffer, greenBuffer, blueBuffer};
-	pixelStruct *arrays[] = {redArray, greenArray, blueArray};
-	copyFromBuffersToArrays(commandQueue, width, height, 3, buffers, arrays);
-
-	//equalize color arrays
-	histEqualization(redArray, height, width);
-	histEqualization(greenArray, height, width);
-	histEqualization(blueArray, height, width);
-
-	//Copy from arrays to buffers
-	copyFromArraysToBuffers(commandQueue,  width, height, 3, buffers, arrays);
-
-	//Create args and run createPixelArray
-	createBuffer(redSortedBuffer, redArray);
-	createBuffer(greenSortedBuffer, greenArray);
-	createBuffer(blueSortedBuffer, blueArray);
-
-	runThisKernel("createPixelArray", globalThreads, localThreads, 
-						   redBuffer, greenBuffer, blueBuffer,
-		                   redSortedBuffer, greenSortedBuffer, blueSortedBuffer,
-						   width);
-
-	//create args and run CreateOutputImage
-	outputImage2D = clCreateImage(context, CL_MEM_WRITE_ONLY, &imageFormat, &imageDesc1, 0, &status);
-    CHECK_OPENCL_ERROR(status,"clCreateImage failed. (outputImage2D)");
-
-	runThisKernel("createOutputImage", globalThreads, localThreads, 
-		                   redSortedBuffer, greenSortedBuffer, blueSortedBuffer,
-						   outputImage2D);
-
-    //Read Output Image to output data
-    size_t origin[] = {0, 0, 0};
-    size_t region[] = {width, height, 1};
-
-	// allocate memory for 2D-copy output image data
-    outputImageData2D = (cl_uchar4*)calloc(width * height, sizeof(cl_uchar4));
-    CHECK_ALLOCATION(outputImageData2D, "Failed to allocate memory! (outputImageData)");
-
-    status = clEnqueueReadImage(commandQueue, outputImage2D, 1, origin, region, 0, 0, outputImageData2D, 0, 0, 0);
-    CHECK_OPENCL_ERROR(status,"clEnqueueReadImage failed.");
-
-	status = clFinish(commandQueue);
-    CHECK_OPENCL_ERROR(status,"clFinish failed.(commandQueue)");
-
-	/*cl_kernel kernelNames[] = {colorArraysKernel, pixelArrayKernel, outputImageKernel};
-	checkResources(3, kernelNames);
-	*/
-    return SDK_SUCCESS;
-}
-
-int SimpleImage::initialize()
-{
-
-    // Call base class Initialize to get default configuration
-    if (sampleArgs->initialize() != SDK_SUCCESS)
-    {
-        return SDK_FAILURE;
-    }
-
-    Option* iteration_option = new Option;
-    CHECK_ALLOCATION(iteration_option,
-                     "Memory Allocation error. (iteration_option)");
-
-    iteration_option->_sVersion = "i";
-    iteration_option->_lVersion = "iterations";
-    iteration_option->_description = "Number of iterations to execute kernel";
-    iteration_option->_type = CA_ARG_INT;
-    iteration_option->_value = &iterations;
-
-    sampleArgs->AddOption(iteration_option);
-
-    delete iteration_option;
-
-    return SDK_SUCCESS;
-}
 
 int SimpleImage::setup()
 {
-    int status = 0;
-
     // create and initialize timers
     int timer = sampleTimer->createTimer();
     sampleTimer->resetTimer(timer);
     sampleTimer->startTimer(timer);
 
-	setupCL("SimpleImage_Kernels.cl");
-
-	//CHECK_OPENCL_ERROR(getInputImage("diplo000000-L.bmp", &imageDesc1, &imageData1), "getInputImage1() failed");
-	//CHECK_OPENCL_ERROR(getInputImage("diplo000000-R.bmp", &imageDesc2, &imageData2), "getInputImage2() failed");
 
     sampleTimer->stopTimer(timer);
     // Compute setup time
@@ -402,28 +148,6 @@ int SimpleImage::setup()
 
 }
 
-int SimpleImage::run()
-{
-    if(!byteRWSupport)
-    {
-        return SDK_SUCCESS;
-    }
-
-    // create and initialize timers
-    int timer = sampleTimer->createTimer();
-    sampleTimer->resetTimer(timer);
-    sampleTimer->startTimer(timer);
-
-    // Set kernel arguments and run kernel
-    CHECK_OPENCL_ERROR(runCLKernels(), "OpenCL run Kernel failed");
-
-	// Compute kernel time
-    sampleTimer->stopTimer(timer);
-    kernelTime = (double)(sampleTimer->readTimer(timer)) / iterations;
-
-
-    return SDK_SUCCESS;
-}
 
 int SimpleImage::cleanup()
 {
@@ -437,25 +161,6 @@ int SimpleImage::cleanup()
     if (clReleaseContext(context) != CL_SUCCESS) throw "clReleaseContext failed.(context)";
     FREE(devices);
 
-    return SDK_SUCCESS;
-}
-
-void SimpleImage::simpleImageCPUReference()
-{
-
-}
-
-int SimpleImage::verifyResults()
-{
-	if(sampleArgs->verify){
-        std::cout << "Verifying 2D copy result - ";
-        // compare the results and see if they match
-        if(!memcmp(imageData1, outputImageData2D, width * height * 4)) {std::cout << "Passed!\n" << std::endl;}
-        else{
-            std::cout << "Failed\n" << std::endl;
-            return SDK_FAILURE;
-        }  
-    }
     return SDK_SUCCESS;
 }
 
@@ -485,13 +190,13 @@ void SimpleImage::printStats()
 
 int main(int argc, char * argv[])
 {
-    int status = 0;
-    SimpleImage clSimpleImage;
-	MyImage imageL; 
-	MyImage imageR;
-
-	try
+    try
 	{
+
+	    SimpleImage clSimpleImage;
+		MyImage imageL; 
+		MyImage imageR;
+	
 		imageL.open("diplo000000-L.bmp");
 		imageR.open("diplo000000-R.bmp");
 
